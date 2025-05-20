d200bdff4eb9bdd95a4f97b7c8579074af1cc95e
Lucas barreto <lpbarreto97@gmail.com>
2025-05-19 21:58:48 -0300

    Codando o meu cadastro
    Fazendo o básico dos imports, alguns talvez não utilize então comento dps

---
+++ clinica/src/main/java/com/mack/clinica/controller/MeuCadastroServlet.java
@@ -0,0 +1,19 @@
+package com.mack.clinica.controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import com.mack.clinica.model.Usuario;
+import com.mack.clinica.model.UsuarioDAO;
+
+
+@WebServlet("/meuCadastro")
+public class MeuCadastroServlet extends HttpServlet {
+    
+}
